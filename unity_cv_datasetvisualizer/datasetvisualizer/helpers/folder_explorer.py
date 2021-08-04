77f209a7dc464c85239fc68c5313c45bb71550dd
leopoldo-zugasti <leopoldo.zugasti@unity3d.com>
2021-08-04 14:13:01 -0400

    Make the file explorer appear above all other windows

--- unity_cv_datasetvisualizer/datasetvisualizer/helpers/folder_explorer.py
+++ unity_cv_datasetvisualizer/datasetvisualizer/helpers/folder_explorer.py
@@ -1,4 +1,5 @@
 ﻿import sys
+from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication, QFileDialog, QWidget
 
 if not QApplication.instance():
@@ -8,7 +9,13 @@ else:
 
 dialog = QFileDialog()
 dialog.setFileMode(QFileDialog.Directory)
+dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+dialog.setWindowState(dialog.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+dialog.activateWindow()
+dialog.setWindowFlags(dialog.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+dialog.show()
 
 if dialog.exec_():
     fileName = dialog.selectedFiles()
     print(fileName[0])
+    sys.exit(app.exec_())

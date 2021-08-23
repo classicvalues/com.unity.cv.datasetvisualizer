f7e1474f3a9ccf8e6545ad90a0a3e17d3de74e8d
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-08-23 11:09:18 -0700

    more streamlit

---
+++ streamlit_components_react_projects/pageselector/pageselector/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import PageSelector from "./PageSelector"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PageSelector />
+  </React.StrictMode>,
+  document.getElementById("root")
+)

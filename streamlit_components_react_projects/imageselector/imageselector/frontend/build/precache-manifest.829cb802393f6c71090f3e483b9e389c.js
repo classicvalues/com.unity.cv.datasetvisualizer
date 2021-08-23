f3b121ba4e1cfeda1fe1f8addfea7d44cdeb0038
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-08-23 10:04:52 -0700

    committing streamlit components

---
+++ streamlit_components_react_projects/imageselector/imageselector/frontend/build/precache-manifest.829cb802393f6c71090f3e483b9e389c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479e7a698fda547c46c99ab197fb35b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31764387fc4c60e9f316",
+    "url": "./static/js/2.d9a90f03.chunk.js"
+  },
+  {
+    "revision": "71aa9e5fe8bf6f2bc9396bc9828f6810",
+    "url": "./static/js/2.d9a90f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b013718f13a3602b88",
+    "url": "./static/js/main.60919b37.chunk.js"
+  },
+  {
+    "revision": "61a45055640fc61ac06a",
+    "url": "./static/js/runtime-main.38c1952d.js"
+  }
+]);
---
+++ streamlit_components_react_projects/imageselector/imageselector/frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import ImageSelector from "./ImageSelector"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ImageSelector />
+  </React.StrictMode>,
+  document.getElementById("root")
+)

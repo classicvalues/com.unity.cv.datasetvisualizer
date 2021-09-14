5c397a532673e28514b3f672dbb4ee9d48f505f6
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-09-14 15:12:28 -0700

    Bumped version for new release

--- unity_cv_datasetvisualizer/setup.py
+++ unity_cv_datasetvisualizer/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="unity-cv-datasetvisualizer",
-    version="0.0.7",
+    version="0.0.8",
     author="Unity Technologies",
     description="This Python based tool allows you to visualize datasets created using Unity Computer Vision tools.",
     long_description=long_description,

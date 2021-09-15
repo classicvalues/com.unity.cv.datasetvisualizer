da3579af0ca57381f1241ca5b44cfa940f3cf758
Mohsen Kamalzadeh <mohsen.kamalzadeh@unity3d.com>
2021-09-14 17:47:29 -0700

    DI version and file dialog change
    - It looks the previous method of showing a folder selection dialog has some issues on OSX, where the dialog sometimes does not show. This commit changes the method used, hopefully fixing that issue.

--- unity_cv_datasetvisualizer/setup.py
+++ unity_cv_datasetvisualizer/setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
         "Pillow>=8.1.0",
         "streamlit==0.84.1",
         "pyquaternion>=0.9.9",
-        "datasetinsights==1.1.0",
+        "datasetinsights==1.1.1",
         "PySide2==5.15.2",
     ],
     entry_points={"console_scripts": ["datasetvisualizer=datasetvisualizer.cli:main"]},

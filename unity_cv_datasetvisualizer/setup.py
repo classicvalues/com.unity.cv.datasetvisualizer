9a4f4afb44e71b89b204c91a76c240524076dedb
Ruiyu Zhnag <etain42@gmail.com>
2022-01-25 20:13:26 -0800

    pkg path

--- unity_cv_datasetvisualizer/setup.py
+++ unity_cv_datasetvisualizer/setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: MacOS",
         "Operating System :: Microsoft :: Windows :: Windows 10"
     ],
-    packages=["datasetvisualizer", "datasetvisualizer/helpers", "datasetvisualizer/visualization"],
+    packages=["unity_cv_datasetvisualizer/datasetvisualizer", "unity_cv_datasetvisualizer/datasetvisualizer/helpers", "unity_cv_datasetvisualizer/datasetvisualizer/visualization"],
     include_package_data=True,
     python_requires=">=3.7, !=3.9.*",
     install_requires=[

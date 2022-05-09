807a02100993ff9f7a8d683236760664f222ddaf
Aryan Mann <aryan.mann@unity3d.com>
2022-05-09 10:12:46 -0700

    dependency causing failing tests

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 
 class InstallationTests(TestCase):
     def test_console_entrypoint(self):
-        from core.cli import entry
+        from datasetvisualizer.core.cli import entry
 
         sys.argv = []
         assert entry() is True
